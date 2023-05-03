ff565c0ba61307742ca0374ad1b4fcfb91d5e86f
M R Ashrit <ashritmr@rocketmail.com>
2023-05-02 21:54:22 -0400

    Add files via upload

---
+++ src/test/java/com/swe645/swe645_Assignment3/Swe645Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe645.swe645_Assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe645Assignment3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
