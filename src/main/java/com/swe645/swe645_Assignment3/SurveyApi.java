f243a735547832e185788f4730fd3e093701e2c8
M R Ashrit <ashritmr@rocketmail.com>
2023-05-04 16:34:30 -0400

    Add files via upload

---
+++ src/main/java/com/swe645/swe645_Assignment3/SurveyApi.java
@@ -0,0 +1,4 @@
+package com.swe645.swe645_Assignment3;
+
+public class SurveyApi {
+}
---
+++ src/main/java/com/swe645/swe645_Assignment3/SurveyRepo.java
@@ -0,0 +1,12 @@
+package com.swe645.swe645_Assignment3;
+
+
+import org.springframework.data.jpa.repository.*;
+
+
+
+
+
+public interface SurveyRepo extends JpaRepository<Surveydata, Integer> {
+
+}
